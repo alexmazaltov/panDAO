78e516ceb552ff238270dbd2937f4a3812ce7124
Oleksii Bondarenko <1605-alex.mazaltov@users.noreply.drupalcode.org>
2024-06-24 00:53:28 +0300

    #1 User Profile created

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,13 +2,13 @@ import { createApp } from "vue"
 import App from './App.vue'
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from './views/Home.vue'
-// import Profile from './views/Profile.vue'
+import Profile from './views/Profile.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: Home },
-    // { path: '/profile', component: Profile }
+    { path: '/profile', component: Profile }
   ]
 })
 
